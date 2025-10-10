f85acc6c1f22496ad570136c402e801339d678f7
Gianmarco Lazzeri <40178659+gl95@users.noreply.github.com>
2025-10-10 10:07:55 +0200

    Create __init__.py

---
+++ aimmd/core/__init__.py
@@ -0,0 +1,5 @@
+from .pathensemble import MDATrajectory
+from .pathensemble import PathEnsemble
+from .pathensemble import PathEnsemblesCollection
+
+__all__ = ['MDATrajectory', 'PathEnsemble', 'PathEnsemblesCollection']
