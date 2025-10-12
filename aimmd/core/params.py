17be699e2beb4097fbe176bde8ba7600d5115127
Gianmarco Lazzeri <40178659+gl95@users.noreply.github.com>
2025-10-12 09:32:14 +0200

    Update params.py

--- aimmd/core/params.py
+++ aimmd/core/params.py
@@ -50,171 +50,263 @@ values."""
 """System's topology (gro file)."""
                  })
     
-    trajectory_extension : str = '.xtc'
-    """Use `xtc` for compressed data, `trr` for full-precision data and
-    also saving velocities along with the positions."""
+    trajectory_extension : str = field(
+        default='.xtc',
+        metadata={'description':
+"""Use `xtc` for compressed data, `trr` for full-precision data and also
+saving velocities along with the positions."""
+                 })
     
-    engine : str = 'gromacs'
-    """Either "gromacs" or "toy" engine. TODO implement others."""
+    engine : str = field(
+        default='gromacs',
+        metadata={'description':
+"""Either "gromacs" or "toy" engine."""
+                 })
     
     # gromacs engine
     
-    gmx_init_mdp : str = 'randomvelocities.mdp'
-    """Used for initializing the shooting point's random velocities.
-    Attention! Velocities will be extracted from trr files."""
+    gmx_init_mdp : str = field(
+        default='randomvelocities.mdp',
+        metadata={'description':
+"""Used for initializing the shooting point's random velocities.
+Attention! Velocities will be extracted from trr files."""
+                 })
     
-    gmx_run_mdp : str = 'run.mdp'
-    """Used for initializing production runs (shooting / free simulations).
-    Attention! It must be consistent with "trajectory_extension"."""
+    gmx_run_mdp : str = field(
+        default='run.mdp',
+        metadata={'description':
+"""Used for initializing production runs (shooting / free simulations).
+Attention! It must be consistent with "trajectory_extension"."""
+                 })
     
-    gmx_grompp : str = f'{GMX} grompp -maxwarn 1'
-    """Gromacs grompp and call options. Change to include index files.
-    Allows to produce `tpr` files. Attention! "coordinates", "restraints",
-    "output", and "nobackup" flags are added automatically, do not include
-    them here."""
+    gmx_grompp : str = field(
+        default=f'{GMX} grompp -maxwarn 1',
+        metadata={'description':
+"""Gromacs grompp and call options. Change to include index files. Allows to
+produce `tpr` files. Attention! "coordinates", "restraints", "output", and
+"nobackup" flags are added automatically, do not include them here."""
+                 })
     
-    gmx_mdrun : str = f'{GMX} mdrun -v -maxh 4'
-    """Gromacs mdrun options. Change to optimize performance. Attention!
-    "deffnm", "nobackup", and "noappend" flags (when required) are added
-    automatically, do not include them here."""
+    gmx_mdrun : str = field(
+        default=f'{GMX} mdrun -v -maxh 4',
+        metadata={'description':
+"""Gromacs mdrun options. Change to optimize performance. Attention! "deffnm",
+"nobackup", and "noappend" flags (when required) are added automatically,
+do not include them here."""
+                 })
     
-    gmx_eneconv : str = f'printf "c\nc\n" | {GMX} -nobackup eneconv -settime'
-    """Command to merge Gromacs "edr" energy files of the backward and forward
-    trajectory segments of a two-way shooting simulations. Leave emtpy in case
-    you do not want to save energy files."""
+    gmx_eneconv : str = field(
+        default=f'printf "c\nc\n" | {GMX} -nobackup eneconv -settime',
+        metadata={'description':
+"""Command to merge Gromacs "edr" energy files of the backward and forward
+trajectory segments of a two-way shooting simulations. Leave emtpy in case
+you do not want to save energy files."""
+                 })
     
     # toy engine
     
-    toy_mdrun : str = f'{PYTHON} integrator.py'
-    """Command to run a custom toy integrator. Attention! It must support
-    the "noappend" flag, and produce trajectory files with the same naming
-    convention as Gromacs."""
+    toy_mdrun : str = field(
+        default=f'{PYTHON} integrator.py',
+        metadata={'description':
+"""Command to run a custom toy integrator. Attention! It must support the
+"noappend" flag, and produce trajectory files with the same naming convention
+as Gromacs."""
+                 })
     
     # other engines will be defined here
     # [...]
     
     # shooting point selection options
     
-    do_tps : bool = False
-    """If True: transition path sampling; if False: rejection-free sampling."""
+    do_tps : bool = field(
+        default=False,
+        metadata={'description':
+"""If True: transition path sampling; if False: rejection-free sampling."""
+                 })
     
-    selection_pool_size : int = 10
-    """Number of candidate paths per selection step. When `do_tps = True`,
-    `selection_pool_size = 1` is the only option."""
+    selection_pool_size : int = field(
+        default=10,
+        metadata={'description':
+"""Number of candidate paths per selection step. When `do_tps = True`,
+`selection_pool_size = 1` is the only option."""
+                 })
     
-    at_least_one_transition_in_pool : bool = False
-    """If True: ensure each pool contains at least 1 transition.
-    This breaks detailed balance, but avoid getting stuck close to the states
-    and compromise exploitation."""
+    at_least_one_transition_in_pool : bool = field(
+        default=False,
+        metadata={'description':
+"""If True: ensure each pool contains at least 1 transition.
+This breaks detailed balance, but avoid getting stuck close to the states
+and compromise exploitation."""
+                 })
     
-    nbins: int = 10
-    """Number of bins partitioning the reactive region according to the
-    logit committor."""
+    nbins: int = field(
+        default=10,
+        metadata={'description':
+"""Number of bins partitioning the reactive region according to the
+logit committor."""
+                 })
     
-    cutoff_max : float = 20.0
-    """Do not exceed `cutoff_max` absolute value with the first and last
-    finite bin boundaries. When not runnig free simulations, bins will
-    span from `-cutoff_max` to `cutoff_max`."""
+    cutoff_max : float = field(
+        default=20.0,
+        metadata={'description':
+"""Do not exceed `cutoff_max` absolute value with the first and last
+finite bin boundaries. When not runnig free simulations, bins will
+span from `-cutoff_max` to `cutoff_max`."""
+                 })
     
-    include_marginal_bins : bool = True
-    """Include two extra bins at A/B boundaries, sacrificing a bit exploitation
-    for exploration. It makes sense only when `selection_pool_size > 1`."""
+    include_marginal_bins : bool = field(
+        default=True,
+        metadata={'description':
+"""Include two extra bins at A/B boundaries, sacrificing a bit exploitation
+for exploration. It makes sense only when `selection_pool_size > 1`."""
+                 })
     
-    lorentzian: float = np.inf
-    """Width of Lorentzian target distribution in logit committor space.
-    If inf: sample shooting points uniformly between first and last bin."""
+    lorentzian: float = field(
+        default=np.inf,
+        metadata={'description':
+"""Width of Lorentzian target distribution in logit committor space.
+If inf: sample shooting points uniformly between first and last bin."""
+                 })
     
-    adjust_selection_in_marginal_bins : bool = True
-    """Try to correct over-selection in first and last bin, without breaking
-    detailed balance."""
+    adjust_selection_in_marginal_bins : bool = field(
+        default=True,
+        metadata={'description':
+"""Try to correct over-selection in first and last bin, without breaking
+detailed balance."""
+                 })
     
-    memory : float = 1.0
-    """Percent of shooting points to keep when computing the bins' populations,
-    used to accelerate the convergence of future shooting points selection."""
+    memory : float = field(
+        default=1.0,
+        metadata={'description':
+"""Percent of shooting points to keep when computing the bins' populations,
+used to accelerate the convergence of future shooting points selection."""
+                 })
     
-    equilibrium_overriding_states : str = 'AB'
-    """Allow occasional shooting point selection from the free simulations
-    around the states in `equilibrium_overriding_states`."""
+    equilibrium_overriding_states : str = field(
+        default='AB',
+        metadata={'description':
+"""Allow occasional shooting point selection from the free simulations
+around the states in `equilibrium_overriding_states`."""
+                 })
     
-    equilibrium_overriding_rate : int = 100
-    """Number of overriding attempts per every selection."""
+    equilibrium_overriding_rate : int = field(
+        default=100,
+        metadata={'description':
+"""Number of overriding attempts per every selection."""
+                 })
     
-    restart_equilibrium_with_transitions : str = ''
-    """States where to use the last frames of a random sampled transition
-    instead of the latest crossing of the target state of the previous free
-    simulation."""
+    restart_equilibrium_with_transitions : str = field(
+        default='',
+        metadata={'description':
+"""States where to use the last frames of a random sampled transition
+instead of the latest crossing of the target state of the previous free
+simulation."""
+                 })
     
     # shooting point initialization options
     
-    randomize_shooting_velocities : bool = True
-    """If True, resample shooting velocities from a Boltzmann distribution as
-    defined in `random_velocities`. Otherwise, reuse velocities from the parent
-    trajectory (works if `trajectory_extension == '.trr'`)."""
+    randomize_shooting_velocities : bool = field(
+        default=True,
+        metadata={'description':
+"""If True, resample shooting velocities from a Boltzmann distribution as
+defined in `random_velocities`. Otherwise, reuse velocities from the parent
+trajectory (works if `trajectory_extension == '.trr'`)."""
+                 })
     
     # simulation options
     
-    max_excursion_length: int = 50_000
-    """Maximum allowed number of frames per path. In this way, avoid the
-    simulations getting stuck in long-lived intermediates."""
+    max_excursion_length: int = field(
+        default=50_000,
+        metadata={'description':
+"""Maximum allowed number of frames per path. In this way, avoid the
+simulations getting stuck in long-lived intermediates."""
+                 })
     
-    extra_equilibriumA : List[str] = field(default_factory=lambda: [])
-    """Add the free simulations in the folders listed in `extra_equilibriumA`
-    to the free simulations produced by this AIMMD run. Useful for recycling
-    already produced data."""
+    extra_equilibriumA : List[str] = field(
+        default_factory=lambda: [],
+        metadata={'description':
+"""Add the free simulations in the folders listed in `extra_equilibriumA`
+to the free simulations produced by this AIMMD run. Useful for recycling
+already produced data."""
+                 })
     
     extra_equilibriumA_states_map : List[str] = field(
-        default_factory=lambda: [''])
-    """When adding the free simulations in the folders listed in
-    `extra_equilibriumA`, for each of these folders, you may need to redefine
-    the states, in case of multi-step transitions. For each string in
-    `extra_equilibriumA_states_map` list, you can do this by adapting the following
-    example. "AB BC ZA" means that A frames become B frames, B become C, and Z
-    become A. If the state is not present in the string, it is not converted."""
+        default_factory=lambda: [''],
+        metadata={'description':
+"""When adding the free simulations in the folders listed in
+`extra_equilibriumA`, for each of these folders, you may need to redefine
+the states, in case of multi-step transitions. For each string in
+`extra_equilibriumA_states_map` list, you can do this by adapting the following
+example. "AB BC ZA" means that A frames become B frames, B become C, and Z
+become A. If the state is not present in the string, it is not converted."""
+                 })
     
-    extra_equilibriumB : List[str] = field(default_factory=lambda: [])
-    """Same as `extra_equilibriumA` but for this run's state B."""
+    extra_equilibriumB : List[str] = field(
+        default_factory=lambda: [],
+        metadata={'description':
+"""Same as `extra_equilibriumA` but for this run's state B."""
+                 })
     
     extra_equilibriumB_states_map : List[str] = field(
-        default_factory=lambda: [''])
-    """Same as `extra_equilibriumB_states_map` but for this run's state B."""
+        default_factory=lambda: [''],
+        metadata={'description':
+"""Same as `extra_equilibriumB_states_map` but for this run's state B."""
+                 })
     
-    extra_extend_frames : int = 0
-    """When performing extension simulations, do not stop right after reaching
-    the target state, but rather continue for furhter `extra_extend_frames`."""
+    extra_extend_frames : int = field(
+        default=0,
+        metadata={'description':
+"""When performing extension simulations, do not stop right after reaching
+the target state, but rather continue for furhter `extra_extend_frames`.""" 
+                 })
     
     # logit committor fit options
     
-    fit : Callable = fit
-    """Default fit function from utils."""
+    fit : Callable = field(
+        default=fit,
+        metadata={'description':
+"""Fit neural network parameters to pathensemble data."""
+                 })
     
-    rescale_committor : bool = True 
-    """If True: rescale the NN output to recover the expected crossing
-    probability behavior ~ `1/p` (from A) and ~ `1/(1 - p)` (from B),
-    where p is the committor, in case of diffusive system with small
-    enough interval between frames."""
+    rescale_committor : bool = field(
+        default=True,
+        metadata={'description':
+"""If True: rescale the NN output to recover the expected crossing
+probability behavior ~ `1/p` (from A) and ~ `1/(1 - p)` (from B),
+where p is the committor, in case of diffusive system with small
+enough interval between frames."""
+                 })
     
     # paths reweighting options 
     
     reweight_parameters : dict = field(
-        default_factory=lambda: {'equilibrium_threshold': 10})
-    """Dictionary of parameters used for reweighting the paths in a
-    `PathEnsemble` or `PathEnsemblesCollection` object, necessary for free
-    energy and rates estimates. Passed to `pathensemble.reweight`."""
+        default_factory=lambda: {'equilibrium_threshold': 10},
+        metadata={'description':
+"""Dictionary of parameters used for reweighting the paths in a
+`PathEnsemble` or `PathEnsemblesCollection` object, necessary for free
+energy and rates estimates. Passed to `pathensemble.reweight`."""
+                 })
     
     # save options
     
-    save_interval : int = 10
-    """Save NN model parameters every N shooting simulations."""
+    save_interval : int = field(
+        default=10,
+        metadata={'description':
+"""Save NN model parameters every N shooting simulations."""
+                 })
     
     # SLURM configuration (for HPC clusters)
     
-    slurm_options : str = ('#SBATCH --ntasks-per-node=1\n'
-                           '#SBATCH --mail-type=FAIL')
-    """Default SLURM configuration. Attention! It must include the
-    ntasks-per-node opton. Do not include #!/bin/bash, job-name, or number
-    of nodes (will be determined automatically). Each two-way shooting and
-    free simulation worker uses one task. Manager and trainer share an extra
-    task together."""
+    slurm_options : str = field(
+        default=('#SBATCH --ntasks-per-node=1\n'
+                 '#SBATCH --mail-type=FAIL'),
+        metadata={'description':
+"""Default SLURM configuration. Attention! It must include the
+ntasks-per-node opton. Do not include #!/bin/bash, job-name, or number
+of nodes (will be determined automatically). Each two-way shooting and
+free simulation worker uses one task. Manager and trainer share an extra
+task together."""
+                 })
     
     # enforce data types when reassigning params
     def __setattr__(self, name, value):
@@ -284,7 +376,8 @@ values."""
                         source = "\n".join(source.splitlines())
                     lines.append(source)
                 except Exception:
-                    lines.append(f"def {name}\n:\n    # source unavailable\n    pass")
+                    lines.append(
+                        f"def {name}\n:\n    # source unavailable\n    pass")
             lines.append('\n')
         
         return "\n".join(lines)
