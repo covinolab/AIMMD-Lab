b319f60e886c48031e7b8e0f4f3356309a98d0b0
Gianmarco Lazzeri <40178659+gl95@users.noreply.github.com>
2025-10-13 16:06:15 +0200

    Create worker.py

---
+++ aimmd/sampling/worker.py
@@ -0,0 +1,134 @@
+import os
+import sys
+import psutil
+import sygnal
+import selectors
+import subprocess
+from ..core.utils import get_current_simulation
+
+def limit_resources(localid=0, cpus_per_task=1, gpus_per_task=0):
+    """Restrict this process to the allocated CPUs and GPUs."""
+    
+    # local id 
+    localid = int(os.getenv("SLURM_LOCALID", f'{localid}'))
+    
+    # CPU binding
+    cpus_per_task = os.getenv("SLURM_CPUS_PER_TASK", f'{cpus_per_task}')
+    if cpus_per_task:
+        try:
+            cpus_per_task = int(cpus_per_task)
+            start = localid * cpus_per_task
+            cpus = list(range(start, start + cpus_per_task))
+            psutil.Process().cpu_affinity(cpus)
+        except Exception as exception:
+            print(f"Warning: could not set CPU affinity ({exception})")
+    
+    # GPU binding
+    start = localid * gpus_per_task
+    gpus_per_task = ','.join([f'{gpu_id}' for gpu_id in range(
+        localid * gpus_per_task, start + gpus_per_task)])
+    gpus_per_task = os.getenv("CUDA_VISIBLE_DEVICES",
+                              gpus_per_task if gpus_per_task else None)
+    os.environ["CUDA_VISIBLE_DEVICES"] = gpus_per_task
+    
+    print(f'Local id: {localid}')
+    print(f'Cores   : {cores}')
+    print(f'CUDA ids: {gpus_per_task}')
+
+
+def run(params, run_file, log_file, append,
+        localid=0, cpus_per_task=1, gpus_per_task=0):
+    """
+    Run and log in real-time on the allocated resources.
+    
+    params: Params class.
+    run_file: File indicating what to simulate.
+    log_file: path to log file.
+    append: Append to existing simulations or start a new part.
+    localid, cpus_per_task, gpus_per_task: resource allocation.
+    """
+    
+    limit_resources(localid, cpus_per_task, gpus_per_task)
+    
+    log = open(log_file, "a+") if log_file else None
+    process = None  # current subprocess
+    
+    def terminate_handler(signum, frame):
+        print(f"Received signal {signum}, terminating...")
+        if log:
+            log.write(f"Received signal {signum}, terminating...")
+        if process and process.poll() is None:
+            process.terminate()
+        sys.exit(0)
+    
+    # register signal handlers
+    signal.signal(signal.SIGTERM, terminate_handler)
+    signal.signal(signal.SIGINT, terminate_handler)
+        
+    try:
+        while True:
+            # get current simulation
+            fname = get_current_simulation(run_file)
+            if not fname:
+                continue  # nothing to run
+            
+            # simulation command
+            command = [
+                params.mdrun, "-deffnm", fname,
+                "-cpo", f"{fname}.cpt",
+                "-cpi", f"{fname}.cpt",
+                "-cpt", ".1"]
+            if not append:
+                command.append('-noappend')
+            
+            # start subprocess
+            process = subprocess.Popen(
+                command,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                text=True,
+                env=os.environ.copy())
+            
+            sel = selectors.DefaultSelector()
+            sel.register(process.stdout, selectors.EVENT_READ)
+            
+            try:
+                while True:
+                    # read stdout
+                    for key, _ in sel.select(timeout=0.1):
+                        line = key.fileobj.readline()
+                        if line:
+                            print(line, end="")
+                            if log:
+                                log.write(line)
+                                log.flush()
+                    
+                    # stop if simulation file changed
+                    if get_current_simulation(run_file) != fname:
+                        process.terminate()
+                        break
+                    
+                    # exit if process finished
+                    if process.poll() is not None:
+                        break
+                
+            finally:
+                process.wait()
+                process = None
+    
+    except KeyboardInterrupt:
+        print(f"[Worker {localid}] Exiting main loop due to interrupt.")
+    
+    finally:
+        if log:
+            log.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 5:
+        print("Usage: worker.py "
+              f"<params_file> <run_file> <log_file> <append> [optional]")
+        print(help(run))
+        sys.exit(1)
+    
+    run(*sys.argv[1:])
