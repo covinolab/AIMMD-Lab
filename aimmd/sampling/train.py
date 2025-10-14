dbc9f706a1f8b74ad8e66e45c25f951230333336
Gianmarco Lazzeri <40178659+gl95@users.noreply.github.com>
2025-10-14 23:42:47 +0200

    Update train.py

--- aimmd/sampling/train.py
+++ aimmd/sampling/train.py
@@ -85,8 +85,8 @@ def train(self, log_file=None, verbose=False, walltime=np.inf):
             pathensemble = shooting_chains + equilibriumA + equilibriumB
             
             print(f'\nTraining the network ({now()})')
-            losses, *_ = fit(network, pathensemble,
-                             initial_paths=initial_paths, verbose=verbose)
+            losses, *_ = fit(network, pathensemble, initial_paths=initial_paths,
+                             verbose=verbose, worker=self)
             if not len(losses):
                 if 'network.h5' in os.listdir(directory):
                     print('!!! reloaded most recent network because training failed')
