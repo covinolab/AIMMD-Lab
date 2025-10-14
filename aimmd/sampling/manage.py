730c9d4d0872e87f104c3ba8c6b3c296508ea30a
Gianmarco Lazzeri <40178659+gl95@users.noreply.github.com>
2025-10-14 23:35:37 +0200

    Update manage.py

--- aimmd/sampling/manage.py
+++ aimmd/sampling/manage.py
@@ -127,7 +127,7 @@ def manage(self, n, nA, nB, eA, eB,
         
         # will wait until the training part has completed at least one cycle
         print(f'\nWaiting for neural network parameters ({now()})')
-        bins, densities = load_network_and_projections(network, directory)
+        bins, densities = load_network_and_projections(network, directory, worker=self)
         
         # update full pathensemble
         pathensemble = shooting_chains + equilibriumA + equilibriumB
