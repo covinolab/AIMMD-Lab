877f9d7d917960bb0460935014141ac44b2c1153
Gianmarco Lazzeri <40178659+gl95@users.noreply.github.com>
2025-10-14 16:09:42 +0200

    Update params.py

--- tests/toy_1d/params.py
+++ tests/toy_1d/params.py
@@ -3,7 +3,9 @@ import torch
 import numpy as np
 from tqdm import tqdm
 
+engine = 'toy'
 topology = 'run.gro'
+trajectory_extension = '.xtc'
 initial_paths = ['initial.xtc']
 
 def descriptors_function(traj, verbose=False):
