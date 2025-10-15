38dba1b36ad437984ec8246d7e89a280207e174e
Gianmarco Lazzeri <40178659+gl95@users.noreply.github.com>
2025-10-16 00:40:14 +0200

    Update params.py

--- tests/retinal/params.py
+++ tests/retinal/params.py
@@ -9,7 +9,7 @@ mdtraj_frame = md.load('run.gro')
 trajectory_extension = '.trr'
 initial_paths = ['initial.trr']
 gmx_grompp = 'gmx grompp -maxwarn 4'
-gmx_mdrun = 'gmx mdrun -v -ntmpi 1 -ntomp 1'
+gmx_mdrun = 'gmx mdrun -v -ntmpi 1'
 
 couples = [(i,j) for i in range(80) for j in range(i+1, 80)]
 
