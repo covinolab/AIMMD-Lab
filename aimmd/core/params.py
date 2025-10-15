2808e85f0eb78bd2e36edc1fb609adf267a8aa37
Gianmarco Lazzeri <40178659+gl95@users.noreply.github.com>
2025-10-15 22:47:48 +0200

    Update params.py

--- aimmd/core/params.py
+++ aimmd/core/params.py
@@ -500,8 +500,8 @@ task together."""
                 # gromacs grompp: mdrun
                 cmd = (f'{self.gmx_grompp} -nobackup -f {self.gmx_run_mdp} '
                        f'-r {self.topology}.gro -c {self.topology}.gro '
-                       f'-o .params_check_engine.tpr') +
-                      (f'-t .params_check_engine.trr if self.random_velocities else '')
+                       f'-o .params_check_engine.tpr') + (
+                       f'-t .params_check_engine.trr if self.random_velocities else '')
                 if exit := os.system(cmd):
                     raise RuntimeError(f'{cmd} failed with exit code {exit}')
 
