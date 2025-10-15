8e2adf365cc648b014564ba94c7f548ec5cad0c7
Gianmarco Lazzeri <40178659+gl95@users.noreply.github.com>
2025-10-15 13:23:30 +0200

    Update worker.py

--- aimmd/sampling/worker.py
+++ aimmd/sampling/worker.py
@@ -151,15 +151,15 @@ class Worker:
         self.process = None
         self.log_file = None
         
-        # exit if required or if worker is a child process
-        if exit:
-            sys.exit(0)
-        
         # kill current process
         if self.is_process:
             parent_pid = os.getpid()  # get PID
             os.kill(parent_pid, signal.SIGTERM)  # send termination signal
             time.sleep(10)
+        
+        # exit if required or if worker is a child process
+        if exit:
+            sys.exit(0)
     
     def run(self, task, *args):
         if task == 'train':
