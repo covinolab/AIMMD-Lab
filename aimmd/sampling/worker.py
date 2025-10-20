4584e80d74311da60e62394ba1c8b510978723e0
Gianmarco Lazzeri <40178659+gl95@users.noreply.github.com>
2025-10-21 01:55:44 +0200

    Update worker.py

--- aimmd/sampling/worker.py
+++ aimmd/sampling/worker.py
@@ -12,7 +12,8 @@ from aimmd.sampling.train import train
 from aimmd.sampling.manage import manage
 from aimmd.sampling.simulate import simulate
 from aimmd.sampling.resources import (get_available_cpus,
-                                      get_available_gpus)
+                                      get_available_gpus,
+                                      bind_resources)
 
 inf = float('inf')
 
@@ -84,72 +85,11 @@ class Worker:
             sys.stdout = open(f'{self.directory}/{log_file}', 'a+')
             sys.stderr = sys.stdout
         self.__log_file = log_file
-
+    
     def bind_resources(self):
-        print(f'Worker\'s resources info')
-        print(f'------------------------')
-        print(f'LocalID {self.localid}')
-        
-        # find available cpus
-        available_cpus = get_available_cpus()
-        
-        # find available gpus, using  to avoid
-        # extra dependency, on cuda or ROCm
-        num_gpus_avail = get_num_gpus()
-        
-        # CPU binding
-        os.environ["OMP_NUM_THREADS"] = str(cpus_per_task)
-        os.environ["MKL_NUM_THREADS"] = str(cpus_per_task)
-        os.environ["OPENBLAS_NUM_THREADS"] = str(cpus_per_task)
-        if len(available_cpus) == cpus_per_task:
-            start = None
-            stop = None
-        else:
-            start = self.localid * cpus_per_task
-            stop = start + cpus_per_task
-        cpus = available_cpus[start:stop]
-        try:
-            psutil.Process().cpu_affinity(cpus)
-        except Exception as exception:
-            print(f"[Warning] Could not set CPU affinity "
-                  f"with {cpus}: {exception}")
-            cpus = []
-        cpus = ",".join([str(id) for id in cpus])
-        
-        # check if requested GPU resources are available
-        if self.gpus_per_task > 0 and num_gpus_avail == 0:
-            raise RuntimeError(f"No GPUs available but {self.gpus_per_task} requested")
-        if self.gpus_per_task > num_gpus_avail:
-            raise RuntimeError(f"Only {num_gpus_avail} GPUs available but "
-                               f"{self.gpus_per_task} requested per task.")
-
-        # GPU binding
-        if self.gpus_per_task > 0:
-            start = self.localid * self.gpus_per_task
-            stop = start + self.gpus_per_task
-            gpus = np.arange(start, stop) % num_gpus_avail
-            gpus = ",".join([str(id) for id in gpus])
-            os.environ["CUDA_VISIBLE_DEVICES"] = gpus
-            # for NVIDIA GPUs, and also ROCm picked up by torch
-            os.environ["GPU_DEVICE_ORDINAL"] = gpus
-            # for ROCm GPUs, Gromacs will use OpenCL
-            
-            # notify the user if this worker is oversubscribing a GPU
-            if stop > num_gpus_avail:
-                print(f"[Note] Worker may be oversubscribing GPUs\n"
-                      f"  available GPUs: {num_gpus_avail}\n"
-                      f"  GPUs per task: {self.gpus_per_task}")
-        
-        # report resource allocation
-        if cpus:
-            print(f"CPU ids: {cpus}")
-        else:
-            print(f"CPU ids: all")
-        if self.gpus_per_task > 0:
-            print(f"GPU ids: {gpus}")
-        else:
-            print(f"No GPUs allocated")
-        print(f'------------------------\n')
+        return bind_resources(self.localid,
+                              self.cpus_per_task,
+                              self.gpus_per_task)
     
     def terminate_handler(self, signum=None, frame=None):
         """Gracefully terminate the worker and its subprocess."""
