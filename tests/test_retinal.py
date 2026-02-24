5c65b057e6f77176471518a90e7404a34c4893c2
Gianmarco Lazzeri <40178659+gl95@users.noreply.github.com>
2026-02-24 09:57:26 +0100

    Update test_retinal.py

--- tests/test_retinal.py
+++ tests/test_retinal.py
@@ -15,14 +15,8 @@ def test_retinal():
     import time
     import aimmd
     import torch
-    from aimmd import PathEnsemble
-    from aimmd.core.utils import update_pathensemble
     import numpy as np
-    import mdtraj as md
-    import matplotlib.pyplot as plt
-    from tqdm import tqdm
-    from scipy.special import logit, expit
-    
+        
     np.set_printoptions(threshold=20)  # better visualization
     
     # run either with pytest from above, or as script in main folder
@@ -30,7 +24,6 @@ def test_retinal():
     tests_dir = current_dir if current_dir.endswith('tests') else current_dir + '/tests'
 
     FOLDER = tests_dir + '/retinal'
-    seconds = 12  # of equilibration
     
     # Attention! Gromacs options come from "retinal/params.py"
     
@@ -45,342 +38,75 @@ def test_retinal():
     print('\nImporting parameters')
     from params import cv
     params = aimmd.Params.load('params.py')
-    params._check_engine()
-    topology = params.topology
-    grompp = params.grompp
-    mdp = params.gmx_run_mdp
+    params.check_engine()
     states_function = params.states_function
     descriptors_function = params.descriptors_function
     values_function = params.values_function
     network = params.network
-    mdrun = params.mdrun
     trajectory_extension = params.trajectory_extension
-    
-    # no max time option here
-    mdrun = mdrun.split()
-    for i, field in enumerate(mdrun):
-        if field == '-maxh':
-            mdrun[i] = ''
-            mdrun[i + 1] = ''
-            break
-    mdrun = ' '.join(mdrun)
-    
-    print(f'\nEquilibrating system for {seconds} seconds')
-    code = os.system(f'{grompp} -f {mdp} '
-              f'-c {topology} -r {topology} '
-              f'-o equilibrium/run.tpr')
-    if code:
-        raise RuntimeError('Error during tpr generation')
-    code = os.system(f'{mdrun} -deffnm equilibrium/run '
-                     f'-maxh {seconds/3600}')
-    if code:
-        raise RuntimeError('Error during simulation')
-    
-    print('\nInitializing equilibrium path ensemble', end='')
-    equilibrium = PathEnsemble(
-        topology=f'../{topology}', directory='equilibrium',
-        states_function=states_function,
-        descriptors_function=descriptors_function,
-        values_function=values_function)
-    print(':', equilibrium)
-    
-    print('\nLoading trajectory from "equilibrium" folder')
-    nframes, times = equilibrium.append(
-        f'run{trajectory_extension}', verbose=True)
-    print(f'Loaded {nframes} frames, time: {times}')
-    if not nframes:
-        raise RuntimeError('Could not load the trajectory file')
-    print('Obtained', equilibrium)
-    
-    print('\nSplitting path ensemble')
-    equilibrium.split()
-    print('Obtained', equilibrium)
-    
-    print('\nSaving path ensemble')
-    equilibrium.save('equilibrium/pathensemble.h5', directory='.')
-    
-    print('(Re)loading path ensemble')
-    # need to reassign functions (no saved in memory)
-    equilibrium = PathEnsemble(
-        states_function=states_function,
-        descriptors_function=descriptors_function,
-        values_function=values_function)
-    equilibrium.load('equilibrium/pathensemble.h5')
-    
-    # report and check that everything goes well
-    inA_indices = np.where((equilibrium.initial_states == 'R') *
-                           (equilibrium.internal_states == 'A') *
-                           (equilibrium.final_states == 'R'))[0]
-    inB_indices = np.where((equilibrium.initial_states == 'R') *
-                           (equilibrium.internal_states == 'B') *
-                           (equilibrium.final_states == 'R'))[0]
-    ARA_indices = np.where((equilibrium.initial_states == 'A') *
-                           (equilibrium.internal_states == 'R') *
-                           (equilibrium.final_states == 'A'))[0]
-    BRB_indices = np.where((equilibrium.initial_states == 'B') *
-                           (equilibrium.internal_states == 'R') *
-                           (equilibrium.final_states == 'B'))[0]
-    TPs_indices = np.where(equilibrium.are_transitions)[0]
-    
-    print('\nStatistics')
-    print('Number of paths  ', len(equilibrium))
-    print('Number of frames ', equilibrium.nframes)
-    print('Lengths          ', equilibrium.lengths)
-    print('Initial states   ', equilibrium.initial_states)
-    print('Shooting states  ', equilibrium.shooting_states)
-    print('Internal states  ', equilibrium.internal_states)
-    print('Final states     ', equilibrium.final_states)
-    print('ARA excursions   ', ARA_indices)
-    print('In A segments    ', inA_indices)
-    print('BRB excursions   ', BRB_indices)
-    print('In B segments    ', inB_indices)
-    print('Transitions      ', TPs_indices)
-    
-    print('\nPlotting time series of selected paths (descriptors/values space)')
-    npaths = 5
-    
-    if len(TPs_indices) and len(equilibrium) > TPs_indices[0] + npaths // 2:
-        i = TPs_indices[-1] - npaths // 2
-    else:
-        i = max(1, len(equilibrium) - npaths)
-    
-    old_label = ''
-    for i in range(i, min(len(equilibrium) - 1, i + npaths)):
-        descriptors = cv(equilibrium.path(i))
-        times = equilibrium.times(i)[0]
-        if i in ARA_indices:
-            color = 'tomato'
-            label = f'ARA'
-        elif i in inA_indices:
-            color = 'firebrick'
-            label = f'inA'
-        elif i in TPs_indices:
-            color = 'black'
-            label = f'TP'
-        elif i in BRB_indices:
-            color = 'dodgerblue'
-            label = f'BRB'
-        elif i in inB_indices:
-            color = 'blue'
-            label = f'inB'
-        if old_label == label:
-            raise RuntimeError(f'Paths {old_label} ({i - 1}) and {label} ({i}) '
-                               f'should be of different kind.')
-        old_label = label
-        plt.figure(1)
-        plt.plot(times[0], descriptors[0], 'o', color='black', zorder=100)
-        plt.plot(times, descriptors, color=color, label=label)
-    
-    plt.figure(1)
-    plt.grid()
-    plt.xlabel('Time [dt]')
-    plt.ylabel('CV')
-    plt.legend()
-    plt.savefig('equilibrium_partial_time_series.pdf')
-    
-    print('\nReweighting (the weights should be constants)')
-    wA, *_ = equilibrium.reweight('A')
-    wB, *_ = equilibrium.reweight('B')
-    print('Weights:', wA + wB)
-    
-    print('\nProjecting the "from A" and "from B" equilibrium ensembles')
-    bins = np.linspace(np.min(equilibrium.frame_descriptors) - 1e-15,
-                       np.max(equilibrium.frame_descriptors) + 1e-15, 11)
-    equilibrium.weights = wA
-    rhoA = equilibrium.project(bins, f=lambda descriptors:descriptors[:, 0])
-    equilibrium.weights = wB
-    rhoB = equilibrium.project(bins, f=lambda descriptors:descriptors[:, 0])
-    print(f'Bins    {bins}  (CV\'s space)')
-    print(f'Centers {(bins[:-1] + bins[1:]) / 2}')
-    print(f'From A  {rhoA}')
-    print(f'From B  {rhoB}')
-    
-    print('\nInitializing AIMMD: three simulators, one manager, one trainer')
-    launcher = aimmd.Launcher(params, 'run1')
-    
-    print('Testing slurm job creation')
-    launcher.create_job('job.sh', n=1, nA=1, nB=1, walltime=3600)
-    # can run it e.g. on cluster
 
     # running one at a time
-    aimmd.Worker(params, 'run1').train(nrounds=1)
-    
+    print('Running one task at a time')
     t0 = time.time()
-    aimmd.Worker(params, 'run1').manage(1, 1, 1, walltime=20)
-    if time.time() - t0 < 20:
-        print('MANAGER FAILED')
+    aimmd.Worker(params, 'run1', walltime=120).train(nrounds=1)
+    if time.time() - t0 > 110:
+        raise RuntimeError(
+            'Training takes too much time, you may want to skip this test')
+    aimmd.Worker(params, 'run1', nframes=100, walltime=120).free(0, 0, 1)
+    aimmd.Worker(params, 'run1', nsteps=5, walltime=120).shoot(1, 0)
     
-    t0 = time.time()
-    aimmd.Worker(params, 'run1').simulate('worker0', noappend=True, walltime=10)
-    if time.time() - t0 < 10:
-        print('FREE A FAILED')
+    print('\nInitializing AIMMD')
+    launcher = aimmd.Launcher('params.py', 'run1')
+    
+    print('Testing slurm job creation')
+    launcher.create_job('job.sh', n=1, n1=1, n2=1, walltime=3600)
+    # can run it e.g. on cluster
 
-    t0 = time.time()
-    aimmd.Worker(params, 'run1').simulate('worker1', noappend=True, walltime=10)
-    if time.time() - t0 < 10:
-        print('FREE B FAILED')
-
-    # worker, trainer, and shooting together
-    launcher.run(n=1, nA=1, nB=1, nsteps=10, walltime=120)
-    
-    print('\nChecking if workers simulated...')
-    file = f'run1/equilibriumA/traj000001.part0001{trajectory_extension}'
-    if not os.path.exists(file) or os.path.getsize(file) <= 68:
-        raise RuntimeError('run1/worker0 failed')
-    print('All fine')
-
-    print('\nTest LaunchersCollection and appending free A')
-    launcher2 = aimmd.Launcher(params, 'run2')
-    os.system('cp run1/network.h5 run2')  # skip first training cycle
-    os.system('cp run1/densities.npy run2')
-    os.system('cp run1/bins.npy run2')
-    launchers = aimmd.LaunchersCollection(launcher, launcher2)
-    launchers.create_job('job.sh', n=[0,1], nA=1, nB=0, walltime=3600)
-    t0 = time.time()
-    launchers.run(n=[0,1], nA=1, nB=0, walltime=30)
-    if time.time() - t0 < 30:
-        print('LauncersCollection RUN FAILED')
-    
-    print('\nChecking if workers simulated...')
-    file = f'run1/equilibriumA/traj000001.part0002{trajectory_extension}'
-    if not os.path.exists(file) or os.path.getsize(file) <= 68:
-        raise RuntimeError('run1/worker0 failed')
-    print('All fine')
+    # together
+    print('Running with launcher')
+    launcher.run(1, 1, 1, nsteps=10, walltime=120)
     
     print('\nLoading AIMMD path ensemble')
-    pathensemble, added_nframes = update_pathensemble('run1', topology,
-        states_function, descriptors_function, values_function,
-        add_missing_paths=False, add_missing_frames=False, verbose=True)
+    pathensemble = params.pathensemble('run1')
+    n_paths = len(pathensemble)
     if not len(pathensemble):
         raise RuntimeError('AIMMD path ensemble still empty!')
     
     print('\nLoading the NN weights and updating the path ensemble values')
-    network.load_state_dict(torch.load('run1/network.h5'))
-    pathensemble.update_values()
-    equilibrium.update_values()
-    
+    network.load_state_dict(torch.load('run1/networkARB.h5'))
+    pathensemble.compute(params.values_function,
+                         'values', 'descriptors', verbose=True)
+        
     # report and check that everything goes well
-    inA_indices = np.where((pathensemble.initial_states == 'R') *
-                           (pathensemble.internal_states == 'A') *
-                           (pathensemble.final_states == 'R'))[0]
-    inB_indices = np.where((pathensemble.initial_states == 'R') *
-                           (pathensemble.internal_states == 'B') *
-                           (pathensemble.final_states == 'R'))[0]
-    ARA_indices = np.where((pathensemble.initial_states == 'A') *
-                           (pathensemble.internal_states == 'R') *
-                           (pathensemble.final_states == 'A'))[0]
-    BRB_indices = np.where((pathensemble.initial_states == 'B') *
-                           (pathensemble.internal_states == 'R') *
-                           (pathensemble.final_states == 'B'))[0]
-    TPs_indices = np.where(pathensemble.are_transitions)[0]
+    i = pathensemble.initial('states')
+    m = pathensemble.middle('states')
+    f = pathensemble.final('states')
+    s = pathensemble.shooting('states')
+    inA_indices = np.flatnonzero((i == 'R') * (m == 'A') * (f == 'R'))
+    inB_indices = np.flatnonzero((i == 'R') * (m == 'B') * (f == 'R'))
+    ARA_indices = np.flatnonzero((i == 'A') * (m == 'R') * (f == 'A'))
+    BRB_indices = np.flatnonzero((i == 'B') * (m == 'R') * (f == 'B'))
+    TPs_indices = np.flatnonzero(pathensemble.are_transitions())
+    are_shot = pathensemble.types('...R')
     
     print('\nStatistics')
     print('Number of paths  ', len(pathensemble))
-    print('Number of shots  ', np.sum(pathensemble.are_shot))
-    print('Number of frames ', pathensemble.nframes)
-    print('Lengths          ', pathensemble.lengths)
-    print('Initial states   ', pathensemble.initial_states)
-    print('Shooting states  ', pathensemble.shooting_states)
-    print('Internal states  ', pathensemble.internal_states)
-    print('Final states     ', pathensemble.final_states)
+    print('Number of shots  ', np.sum(are_shot))
+    print('Number of frames ', sum(pathensemble.n_frames))
+    print('Lengths          ', pathensemble.n_frames)
+    print('Initial states   ', i)
+    print('Shooting states  ', s)
+    print('Middle states    ', m)
+    print('Final states     ', f)
     print('ARA excursions   ', ARA_indices)
     print('In A segments    ', inA_indices)
     print('BRB excursions   ', BRB_indices)
     print('In B segments    ', inB_indices)
     print('Transitions      ', TPs_indices)
     
-    print('\nPlotting time series of selected paths (descriptors/values space)')
-    
-    if len(TPs_indices) and len(pathensemble) > TPs_indices[0] + npaths // 2:
-        i = TPs_indices[-1] - npaths // 2
-    else:
-        i = max(0, len(pathensemble) - npaths)
-    
-    for i in range(i, min(len(pathensemble), i + npaths)):
-        descriptors = cv(pathensemble.path(i))
-        values = expit(pathensemble.values(i)[0])  # committor
-        times = np.arange(len(values))
-        shooting_index = pathensemble.shooting_indices[i]
-        if i in ARA_indices:
-            color = 'tomato'
-            label = f'ARA'
-        elif i in inA_indices:
-            color = 'firebrick'
-            label = f'inA'
-        elif i in TPs_indices:
-            color = 'black'
-            label = f'TP'
-        elif i in BRB_indices:
-            color = 'dodgerblue'
-            label = f'BRB'
-        elif i in inB_indices:
-            color = 'blue'
-            label = f'inB'
-        
-        plt.figure(2)
-        plt.plot(times[shooting_index],
-                 descriptors[shooting_index], 'o', color='black', zorder=100)
-        plt.plot(times, descriptors, color=color, label=label)
-        
-        plt.figure(3)
-        plt.plot(times[shooting_index],
-                 values[shooting_index], 'o', color='black', zorder=100)
-        plt.plot(times, values, color=color, label=label)
-    
-    plt.figure(2)
-    plt.grid()
-    plt.xlabel('Time [dt]')
-    plt.ylabel('x coordinate')
-    plt.legend()
-    plt.savefig('aimmd_descriptors_partial_time_series.pdf')
-    
-    plt.figure(3)
-    plt.grid()
-    plt.xlabel('Time [dt]')
-    plt.ylabel('Estimated committor')
-    plt.legend()
-    plt.savefig('aimmd_values_partial_time_series.pdf')
-    
-    print('\nReweighting and estimating the rates')
-    wA, *_ = pathensemble.reweight('A')
-    wB, *_ = pathensemble.reweight('B')
-    print('Weights:', wA + wB)
-    kAB = np.nan_to_num(1 / np.sum(wA * pathensemble.internal_lengths))
-    kBA = np.nan_to_num(1 / np.sum(wB * pathensemble.internal_lengths))
-    print(f'kAB estimate: {kAB:.3e} [1/dt]')
-    print(f'kBA estimate: {kBA:.3e} [1/dt]')
-    
-    print('\nProjecting the "from A" and "from B" ensembles')
-    bins = np.linspace(np.min(pathensemble.frame_values) - 1e-15,
-                       np.max(pathensemble.frame_values) + 1e-15, 11)
-    pathensemble.weights = wA
-    rhoA = pathensemble.project(bins)
-    pathensemble.weights = wB
-    rhoB = pathensemble.project(bins)
-    
-    print(f'Bins    {bins}  (logit committor)')
-    print(f'Centers {(bins[:-1] + bins[1:]) / 2}')
-    print(f'From A  {rhoA}  (AIMMD reweighted)')
-    
-    wA, *_ = equilibrium.reweight('A')
-    equilibrium.weights = wA
-    rhoA = equilibrium.project(bins)
-    
-    print(f'        {rhoA}  (equilibrium)')
-    
-    print(f'From B  {rhoB}  (AIMMD reweighted)')
-    
-    wB, *_ = equilibrium.reweight('B')
-    equilibrium.weights = wB
-    rhoB = equilibrium.project(bins)
-    
-    print(f'        {rhoB}  (equilibrium)')
-    
     print(f'\nTesting trajectory file-based projections')
-    pathensemble.project([0, 1, 2], f=cv, frames=True)
+    print(pathensemble.project([0, 1, 2, 3, 4], function=cv, source='reader'))
     print('Done!')
-
-
 
 if __name__ == '__main__':
     test_retinal()
