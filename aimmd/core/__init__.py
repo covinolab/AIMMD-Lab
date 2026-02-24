dc415e8365cccb3bb1fbc4a36c23efa424ed6527
Gianmarco Lazzeri <40178659+gl95@users.noreply.github.com>
2026-02-24 10:25:41 +0100

    Update __init__.py

--- aimmd/core/__init__.py
+++ aimmd/core/__init__.py
@@ -1,11 +1,3 @@
-from .params import Params
-from .pathensemble import MDATrajectory
-from .pathensemble import PathEnsemble
-from .pathensemble import PathEnsemblesCollection
-
-__all__ = [
-    'MDATrajectory',
-    'PathEnsemble',
-    'PathEnsemblesCollection',
-    'Params'
-]
+"""
+...
+"""
