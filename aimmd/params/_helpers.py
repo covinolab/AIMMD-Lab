dc8808276341eb7f1f5309b5c1986397d065d222
Gianmarco Lazzeri <40178659+gl95@users.noreply.github.com>
2026-02-25 10:28:26 +0100

    Bugfix when using .update method

--- aimmd/params/_helpers.py
+++ aimmd/params/_helpers.py
@@ -111,8 +111,9 @@ class ParamsHelpers(ABC):
             
             # initial paths (converted in real-time)
             elif name == 'initial_paths':
-                self.__dict__['_reload_initial_paths'] = \
-                    not isinstance(value, (Path, PathEnsemble))
+                if process_and_check:  # otw bug in reloading paths
+                    self.__dict__['_reload_initial_paths'] = \
+                        not isinstance(value, (Path, PathEnsemble))
                 initial_paths = PathEnsemble(value)
                 if value and not initial_paths:
                     raise TypeError(
