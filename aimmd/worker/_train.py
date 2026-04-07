ba86573be03490ea8153448cbcf76c0fccb0b8d5
Gianmarco Lazzeri <40178659+gl95@users.noreply.github.com>
2026-04-07 11:47:23 +0200

    Pruned useless imports

--- aimmd/worker/_train.py
+++ aimmd/worker/_train.py
@@ -66,9 +66,7 @@ import torch
 import numpy as np
 import shutil
 from abc import ABC
-from glob import glob
 from math import inf, nan
-from tqdm import tqdm
 
 # aimmd imports
 from .utils import rescale_bins
@@ -77,12 +75,11 @@ from ..cache.npy import save_npy
 from ..core.utils import now, replace_in_cache
 from ..pathensemble import PathEnsemble
 from ..analysis.utils import compute_bins
-from ..execute.threads import ThreadExecutor
 from ..pathensemble.utils import assemble_pathensemble
 from ..network.rescale_utils import find_knots_and_values, rescale
 from ..path.utils import get_cache_fname
 
-
+# WorkerTrain mixin class
 class WorkerTrain(ABC):
 
     def train(self, nrounds=1, keep_running=False, **kwargs):
